f670b4fc45db7b992b956c794a89f7afda145ce7
Raditya Maulana <radityamaulana27@gmail.com>
2025-01-24 10:13:16 +0700

    Add Tugas-14

---
+++ Tugas-14/Tugas12/resources/views/welcome.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.master')
+@section('title')
+    Halaman Welcome
+@endsection
+@section('content')
+    <h1>SELAMAT DATANG, {{ $firstName }} {{ $lastName }}!</h1>
+    <h2>Terima kasih telah bergabung di SanberBook. Social Media kita bersama!</h2>
+@endsection
